b1bf522e6aaf7b8dc81b198d780ae187f9760a6a
Tyler Grasee <tgrasee@gmail.com>
2025-01-13 17:20:48 -0500

    async, const id, update const post to id from params.id

--- blog/src/app/blog/post/[id]/page.tsx
+++ blog/src/app/blog/post/[id]/page.tsx
@@ -1,10 +1,17 @@
 import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
 
-export default function BlogPosts({ params }: { params: { id: string } }) {
+export default async function BlogPosts({ params }: { params: { id: string } }) {
+
+    // Await the params before using its properties
+    const { id } = await params;
 
     // Find the post with the id that matches the URL parameter
-    const post = posts.find((post) => post.id === params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+        return <div>Post not found</div>;
+    }
     return (
         <div>
             <h1 className='text-4xl text-gray-700 font-bold mb-4'>Post</h1>
