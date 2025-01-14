a90c5e0543b3aebdf6105392648cc89b10252716
Tyler Grasee <tgrasee@gmail.com>
2025-01-13 19:07:21 -0500

    remove JSX from import

--- blog/src/app/ui/components/posts/Post.tsx
+++ blog/src/app/ui/components/posts/Post.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import React, { JSX } from 'react';
+import React from 'react';
 
 export default function Component({ id, title, content, date }: { id: string, title: string, content: string, date: string }) {
     return (
